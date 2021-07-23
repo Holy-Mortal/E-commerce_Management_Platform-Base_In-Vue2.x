b706639a9b2ab3e71b51f331073791e35d4d8a03
Holy-Mortal <holy_mortal@163.com>
2021-07-23 14:41:55 +0800

    分类参数开发功能完毕

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,8 @@ import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
+
 
 
 Vue.use(VueRouter)
@@ -32,7 +34,8 @@ const routes = [
             { path: '/users', component: Users },
             { path: '/rights', component: Rights },
             { path: '/roles', component: Roles },
-            { path: '/categories', component: Cate }
+            { path: '/categories', component: Cate },
+            { path: '/params', component: Params },
         ]
     }
 ]
