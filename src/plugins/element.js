242226c6246931ae56e458f9a2d5b328b216ea6e
Holy-Mortal <holy_mortal@163.com>
2021-07-23 22:11:16 +0800

    商品功能开发完毕

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -39,6 +39,11 @@ import {
     Alert,
     Tabs,
     TabPane,
+    Steps,
+    Step,
+    Checkbox,
+    CheckboxGroup,
+    Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -72,5 +77,10 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import GoodsList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 
 
 
@@ -36,6 +38,8 @@ const routes = [
             { path: '/roles', component: Roles },
             { path: '/categories', component: Cate },
             { path: '/params', component: Params },
+            { path: '/goods', component: GoodsList },
+            { path: '/goods/add', component: Add },
         ]
     }
 ]
